933c0b45f034d04beb6264097fdc64fc8b938ce1
apinayanxyz <akanenthirarasa@gmail.com>
2024-05-06 16:18:50 +0100

    Initial commit

---
+++ app/src/main/java/com/example/tippy/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.tippy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = MainActivity.class.getSimpleName();
+
+    private EditText etBaseAmount;
+    private SeekBar seekBarTip;
+    private TextView tvTipPercentLabel;
+    private TextView tvTipAmount;
+    private TextView tvTotalAmount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        etBaseAmount = findViewById(R.id.etBaseAmount);
+        seekBarTip = (SeekBar)findViewById(R.id.seekBarTip);
+        tvTipPercentLabel = findViewById(R.id.tvTipPercentageLabel);
+        tvTipAmount = findViewById(R.id.tvTipAmount);
+        tvTotalAmount = findViewById(R.id.tvTotalLabel);
+
+        seekBarTip.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser){
+
+                        Log.i(TAG  , "onProgressChanged " + progress);
+                    }
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar){
+
+                    }
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekbar){
+
+                    }
+        });
+    }
+}
